ba623c499bb8ce60e241c6b7872a13b96b36dc44
louvyo <miftahulh654@gmail.com>
2024-10-10 19:28:08 +0800

    sidebar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -18,14 +18,14 @@
                     </svg>
                 </button>
                 <a href="/" class="flex ml-2 md:mr-24">
-                    <img src='asset/LOGO PUPR - 2023.png' class="h-8 mr-3" alt="LOGO PUPR" />
+                    <img src='asset/LOGO PUPR - 2023.png' class="h-9 mr-3" alt="LOGO PUPR" />
                     <span
-                        class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">SIMPEG</span>
+                        class="self-center text-2xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">SIMPEG</span>
                 </a>
             </div>
 
             <aside id="sidebar"
-                class="fixed top-0 left-0 z-20 flex flex-col flex-shrink-0 w-64 h-full pt-16 font-normal duration-75 lg:flex transition-width"
+                class="fixed top-0 left-0 z-25 flex flex-col flex-shrink-0 w-64 h-full pt-16 font-normal duration-75 lg:flex transition-width"
                 aria-label="Sidebar">
                 <div
                     class="relative flex flex-col flex-1 min-h-0 pt-0 bg-white border-r border-gray-200 dark:bg-gray-800 dark:border-gray-700">
@@ -34,25 +34,6 @@
                         <div
                             class="flex-1 px-3 space-y-1 bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
                             <ul class="pb-2 space-y-2">
-                                <li>
-                                    <form action="#" method="GET" class="lg:hidden">
-                                        <label for="mobile-search" class="sr-only">Search</label>
-                                        <div class="relative">
-                                            <div
-                                                class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                                                <svg class="w-5 h-5 text-gray-500" fill="currentColor"
-                                                    viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                                    <path fill-rule="evenodd"
-                                                        d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                                                        clip-rule="evenodd"></path>
-                                                </svg>
-                                            </div>
-                                            <input type="text" name="email" id="mobile-search"
-                                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-200 dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                                                placeholder="Search" />
-                                        </div>
-                                    </form>
-                                </li>
                                 <li>
                                     <a href={url()}
                                         class="flex items-center p-2 text-base text-gray-900 rounded-lg hover:bg-gray-100 group dark:text-gray-200 dark:hover:bg-gray-700">
@@ -72,13 +53,13 @@
                                 <li>
                                     <button type="button"
                                         class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700"
-                                        aria-controls="dropdown-layouts" data-collapse-toggle="dropdown-layouts">
-                                        <svg class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
-                                            fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"
-                                            aria-hidden="true">
-                                            <path
-                                                d="M3 4a1 1 0 011-1h12a1 1 0 011 1v2a1 1 0 01-1 1H4a1 1 0 01-1-1V4zM3 10a1 1 0 011-1h6a1 1 0 011 1v6a1 1 0 01-1 1H4a1 1 0 01-1-1v-6zM14 9a1 1 0 00-1 1v6a1 1 0 001 1h2a1 1 0 001-1v-6a1 1 0 00-1-1h-2z">
-                                            </path>
+                                        aria-controls="dropdown-pegawai" data-collapse-toggle="dropdown-pegawai">
+                                        <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
+                                            xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                            fill="currentColor" viewBox="0 0 24 24">
+                                            <path fill-rule="evenodd"
+                                                d="M8 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4H6Zm7.25-2.095c.478-.86.75-1.85.75-2.905a5.973 5.973 0 0 0-.75-2.906 4 4 0 1 1 0 5.811ZM15.466 20c.34-.588.535-1.271.535-2v-1a5.978 5.978 0 0 0-1.528-4H18a4 4 0 0 1 4 4v1a2 2 0 0 1-2 2h-4.535Z"
+                                                clip-rule="evenodd" />
                                         </svg>
                                         <span class="flex-1 ml-3 text-left whitespace-nowrap"
                                             sidebar-toggle-item>Pegawai</span>
@@ -89,18 +70,25 @@
                                                 clip-rule="evenodd"></path>
                                         </svg>
                                     </button>
-                                    <ul id="dropdown-layouts" class="hidden py-2 space-y-2">
+                                    <ul id="dropdown-pegawai" class="hidden py-2 space-y-2">
                                         <li>
-                                            <a href='#'
-                                                class="flex items-center p-2 text-base text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700">Stacked</a>
+                                            <a href="#"
+                                                class="flex items-center p-2 text-base text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700">Personal</a>
                                         </li>
                                         <li>
-                                            <a href='#'
-                                                class="flex items-center p-2 text-base text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700">Sidebar</a>
+                                            <a href="#"
+                                                class="flex items-center p-2 text-base text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700">Gaji</a>
+                                        </li>
+                                        <li>
+                                            <a href="#"
+                                                class="flex items-center p-2 text-base text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700">Cuti</a>
+                                        </li>
+                                        <li>
+                                            <a href="#"
+                                                class="flex items-center p-2 text-base text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700">Golongan</a>
                                         </li>
                                     </ul>
                                 </li>
-
                             </ul>
 
                         </div>
