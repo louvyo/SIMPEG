8180e0b95e8741a0858f32f29a1cb9bb2131d908
louvyo <miftahulh654@gmail.com>
2024-10-11 15:17:41 +0800

    sidebar 1/2

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -37,16 +37,7 @@
                                 <li>
                                     <a href={url()}
                                         class="flex items-center p-2 text-base text-gray-900 rounded-lg hover:bg-gray-100 group dark:text-gray-200 dark:hover:bg-gray-700">
-                                        <svg fill="currentColor"
-                                            class="w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
-                                            viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                                            <path
-                                                d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z">
-                                            </path>
-                                            <path
-                                                d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z">
-                                            </path>
-                                        </svg>
+                                        <i class="fa-solid fa-house"></i>
                                         <span class="ml-3" sidebar-toggle-item>Home</span>
                                     </a>
                                 </li>
@@ -54,13 +45,7 @@
                                     <button type="button"
                                         class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700"
                                         aria-controls="dropdown-pegawai" data-collapse-toggle="dropdown-pegawai">
-                                        <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
-                                            xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                            fill="currentColor" viewBox="0 0 24 24">
-                                            <path fill-rule="evenodd"
-                                                d="M8 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4H6Zm7.25-2.095c.478-.86.75-1.85.75-2.905a5.973 5.973 0 0 0-.75-2.906 4 4 0 1 1 0 5.811ZM15.466 20c.34-.588.535-1.271.535-2v-1a5.978 5.978 0 0 0-1.528-4H18a4 4 0 0 1 4 4v1a2 2 0 0 1-2 2h-4.535Z"
-                                                clip-rule="evenodd" />
-                                        </svg>
+                                        <i class="fa-solid fa-user-group"></i>
                                         <span class="flex-1 ml-3 text-left whitespace-nowrap"
                                             sidebar-toggle-item>Pegawai</span>
                                         <svg sidebar-toggle-item class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20"
@@ -77,17 +62,16 @@
                                         </li>
                                         <li>
                                             <a href="#"
-                                                class="flex items-center p-2 text-base text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700">Gaji</a>
-                                        </li>
-                                        <li>
-                                            <a href="#"
                                                 class="flex items-center p-2 text-base text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700">Cuti</a>
                                         </li>
-                                        <li>
-                                            <a href="#"
-                                                class="flex items-center p-2 text-base text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700">Golongan</a>
-                                        </li>
                                     </ul>
+                                </li>
+                                <li>
+                                    <a href={url()}
+                                        class="flex items-center p-2 text-base text-gray-900 rounded-lg hover:bg-gray-100 group dark:text-gray-200 dark:hover:bg-gray-700">
+                                        <i class="fa-solid fa-clock-rotate-left"></i>
+                                        <span class="ml-3" sidebar-toggle-item>Riwayat</span>
+                                    </a>
                                 </li>
                             </ul>
 
